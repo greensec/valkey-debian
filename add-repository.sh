e44c160293b3ea245e65e3089b4070e9bec30390
Stefan Meinecke <meinecke@greensec.de>
2026-03-01 21:37:07 +0100

    Revert "add trixie support."
    This reverts commit 51770d679e62679c308d923834800078331ded3b.

--- add-repository.sh
+++ add-repository.sh
@@ -7,6 +7,6 @@ fi
 
 ${SUDO} apt-get update
 ${SUDO} apt-get -y install lsb-release ca-certificates wget
-${SUDO} wget -O /usr/share/keyrings/greensec.github.io-valkey-debian.gpg https://greensec.github.io/valkey-debian/public.gpg
-echo "deb [signed-by=/usr/share/keyrings/greensec.github.io-valkey-debian.gpg] https://greensec.github.io/valkey-debian/repo $(lsb_release -sc) main" | ${SUDO} tee /etc/apt/sources.list.d/valkey-debian.list
+${SUDO} wget -O /usr/share/keyrings/greensec.github.io-valkey-debian.key https://greensec.github.io/valkey-debian/public.key
+echo "deb [signed-by=/usr/share/keyrings/greensec.github.io-valkey-debian.key] https://greensec.github.io/valkey-debian/repo $(lsb_release -sc) main" | ${SUDO} tee /etc/apt/sources.list.d/valkey-debian.list
 ${SUDO} apt-get update
